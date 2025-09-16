6c30df2635a5400cef3f3bee4c613ddd15cb45f4
Lorrana1712 <lorrana.b.santos@aluno.senai.br>
2025-09-16 15:44:46 -0300

    operadores

---
+++ Atividade02/Program.cs
@@ -0,0 +1,33 @@
+﻿//Operadores Aritmeticos
+
+/*
+ + = Adição
+ - = Subtração
+ * = Multiplicação
+ / = Divisão
+
+*/
+
+//Entraa de Dados
+int soma = 2 + 2;
+int subtracao = 10 - 5;
+int multiplicacao = 3 * 7;
+double divisao = 3 / 2.5;
+
+//Saída de Dados 
+Console.WriteLine($"2 +2 = {soma}");
+Console.WriteLine($"10 - 5 = {subtracao}");
+Console.WriteLine($"3 * 7 = {multiplicacao}");
+Console.WriteLine($"3 / 2.5 = {divisao}");
+
+//Ultilizando Operadores Aritmétricos com Varíaveis 
+int num1, num2, resultado;
+
+Console.WriteLine("Insira a primeira Nota");
+num1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Insira a segunda Nota");
+num2 = Convert.ToInt16(Console.ReadLine());
+
+resultado = num1 + num2;
+Console.WriteLine($"A soma das Notas é :{resultado}");
